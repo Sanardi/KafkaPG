403a62773221191ddf9931f30088c1f2c0d521b5
sanardiazam <rubypromise@gmx.com>
2021-05-26 15:07:23 +0100

    added tests

---
+++ test/test_KafkaPG.py
@@ -0,0 +1,47 @@
+import unittest
+import kafkaPG
+import dbcon
+import marziasconsumer
+import marziasproducer
+from psycopg2.extras import RealDictCursor
+import psycopg2
+
+class KafkaPGOnlineTests(unittest.TestCase):
+    """Unit tests for KafkaPG.py that require an internet connection"""
+
+    #def test_database_version1(self):
+        #sql = 'SELECT version()'
+        #version = dbcon.connect(sql)
+        #self.assertIn(b'12.6', version)
+
+    #def test_database_version2(self):
+        #sql = 'SELECT version()'
+        #version = dbcon.make_con(sql)
+        #self.assertIn(b'12.6', version)
+
+    def test_request1(self):
+        data_expected = 200
+        test_url = kafkaPG.KafkaPG(url = "https://google.com")
+        test_dict = test_url.make_request()
+        data_diff = test_dict["code"]
+        self.assertEqual(data_diff, data_expected)
+
+    def test_request2(self):
+        data_expected = 404
+        test_url = kafkaPG.KafkaPG(url = "https://example.com/funny/this-should-not-exist/blafasel")
+        test_dict = test_url.make_request()
+        data_diff = test_dict["code"]
+        self.assertEqual(data_diff, data_expected)
+
+    def test_db(self):
+
+        uri = "postgres://avnadmin:gmr7ls2v4zqjdqis@pg-marzia-azam-9f68.aivencloud.com:28386/defaultdb?sslmode=require"
+        db_conn = psycopg2.connect(uri)
+        c = db_conn.cursor(cursor_factory=RealDictCursor)
+        c.execute("SELECT 1 = 1")
+        result = c.fetchone()
+        self.assertIn("True", result)
+
+
+if __name__ == '__main__':
+    unittest.main(buffer=True)
